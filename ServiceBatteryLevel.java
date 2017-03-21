c1977fb28cb82971d3227928f33a9964b0c45e13
AmineAbdelli <amineabdelli@gmail.com>
2017-03-21 21:45:16 +0100

    Add files via upload

---
+++ ServiceBatteryLevel.java
@@ -0,0 +1,54 @@
+package pciom.projet_ssio;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+
+public class ServiceBatteryLevel extends Service {
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        while (true) {
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    batteryLevel();
+                    Log.e("Status changed", "Status changed");
+                }
+            }, 5000);
+            return START_STICKY;
+        }
+    }
+
+    private void batteryLevel() {
+        BroadcastReceiver batteryLevelReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                context.unregisterReceiver(this);
+                int rawlevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+                int level = -1;
+                if (rawlevel >= 0 && scale > 0) {
+                    level = (rawlevel * 100) / scale;
+                }
+                AsyncT.getInstance().batteryLevel=""+level;
+                Log.e("Batarry status iss", level + "mm");
+            }
+        };
+        IntentFilter batteryLevelFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(batteryLevelReceiver, batteryLevelFilter);
+        return;
+    }
+}
+
